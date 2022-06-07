64991dd5844f96d42071565a06c823641895b443
Chris <me@chrissherlock.co.uk>
2022-06-07 19:11:30 +0100

    Bump to Vue3

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-// import VueRouter from 'vue-router'
+import * as Vue from 'vue'
 import App from './App.vue'
 import '../assets/css/index.css';
 
-Vue.config.productionTip = false
+import { createApp } from 'vue';
+
+// Vue.config.productionTip = false
 
 // Vue.use(VueRouter)
 
-new Vue({
-    // router,
-    render: h => h(App),
-  }).$mount('#app')
+const app = createApp(App);
+
+app.mount('#app');
   
